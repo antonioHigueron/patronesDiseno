904eff5b5bfaa9e106556922fc816f8f2c8ad928
antonioHigueron <acobosscabello@gmail.com>
2023-03-10 22:48:39 +0100

    abstract_factory url

--- proyecto/src/main/java/org/example/Main.java
+++ proyecto/src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Abstract_Factory: https://refactoring.guru/es/design-patterns/abstract-factory/java/example
+
     }
 }
